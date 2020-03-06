71458e17cffd6023cf454858aba860c3bbf2ae3d
Raul Tedesco <raul.a.tedesco@gmail.com>
2020-03-06 20:12:34 -0300

    more chanches

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/raul/development/python/django_proyects/dra.abud/medsys_pediatria/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
